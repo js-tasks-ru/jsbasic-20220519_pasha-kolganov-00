b07c312debbb72177ed3c9978235df31789a6c5f
Pavel Kolganov <pasha.kolganov.00@gmail.com>
2022-06-02 14:17:59 +0300

    Фильтрация по диапазону

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,11 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  
+  let newArr = [];
+
+  for(let i in arr){
+    if(arr[i] >= a && arr[i] < b){
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 }
