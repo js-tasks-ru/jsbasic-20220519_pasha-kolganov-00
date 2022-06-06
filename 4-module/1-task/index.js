d2183f2397dd177e4453047dab475b329a129dbf
Pavel Kolganov <pasha.kolganov.00@gmail.com>
2022-06-06 18:35:26 +0300

    Преобразовние массива друзей в стандартный HTML список

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
   // ваш код...
+  const ul = document.createElement('ul');
+
+  for(let name of friends){
+
+    ul.innerHTML += `<li> ${name.firstName} ${name.lastName}</li>`;
+
+
+  }
+  return ul;
+
 }
