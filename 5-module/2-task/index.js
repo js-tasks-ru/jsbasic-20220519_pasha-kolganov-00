7e676c42319d5a8188f8c1c3e1232fd98684621c
Pavel Kolganov <pasha.kolganov.00@gmail.com>
2022-06-12 14:57:16 +0300

    Кнопка скрывается и возвращается

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,18 @@
 function toggleText() {
-  // ваш код...
+  
+
+
+  let button = document.querySelector(".toggle-text-button");
+  let text = document.querySelector("#text");
+
+  function but(){
+    if(text.hidden == false){
+      text.hidden = true;
+    }else{
+      text.hidden = false;
+    }
+  }
+
+  button.addEventListener("click",but);
+
 }
